c258733f162f7b168ab1c94c57267db89e994ffa
Manish <manishkasera456@gmail.cm>
2021-02-02 15:57:24 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+  // //size
+  // int arr[5];
+  // cout << "Enter five numbers:";
+  // //user se 5 no. le
+  // for (int i = 0; i < 5; i++)
+  // {
+  //   cin >> arr[i];
+  // }
+
+  // //sum of all numbers
+  // int sum = 0;
+  // for (int i = 0; i < 5; i++)
+  // {
+  //   sum += arr[i];
+  // }
+
+  // //avg = sum/size;
+  // float avg = sum / 5.0;
+  // cout << avg;
+  //array is a continuous block and having same data type..
+  // array initilization
+
+  int arr[4]={1,2,3,4}; 
+  for(int i=0;i<4;i++){
+    cout<<arr[i]<<" ";
+  }
+
+  //  int x[4];
+  //  x[0] = 2;
+  //  x[1] = 21;
+  //  x[2] = 222;
+  //  x[3] = 2;
+  //  cout<<x[0]<<endl;
+  //  cout<<x[2]<<endl;
+  //  cout<<x[3]<<endl;
+  //  cout<<x[1]<<endl;
+
+  // cout<<"Enter four numbers:";
+  // int x[4];
+  // for(int i=0;i<4;i++){
+  //   cin>>x[i];
+  // }
+  // cout<<endl;
+
+  // for(int i=0;i<4;i++){
+  //   cout<<x[i]<<" ";
+  // }
+
+  // int arr[5];
+  // cout<<"Enter five numbers:";
+  // for(int i=0;i<5;i++){
+  //   cin >>arr[i];
+  // }
+
+  // float sum = 0;
+  // for(int i=0;i<5;i++){
+  //   sum+=arr[i];
+  // }
+  // float avg=sum/5.0;
+  // cout<<avg;
+
+  return 0;
+}
